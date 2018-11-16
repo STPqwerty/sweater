c7591b756c26c49da3701af679101d186c94eba3
alex <akuzin20@gmail.com>
2018-11-16 15:29:45 +0300

    init v3 (+ фильтр, редактирован запрос базы в пропертях)

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -5,10 +5,12 @@ import com.example.sweater.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 //програмный модуль, который слушает запрос по пути - /greeting
@@ -41,6 +43,19 @@ public class GreetingController {
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+//если фильтр не задан или пуст то ищем по тегу
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);//findbyTag возвр лист
+        }else{
+            messages = messageRepo.findAll();//findAll возвращ Iterable
+        }
         model.put("messages", messages);
         return "main";
     }
--- src/main/java/com/example/sweater/repository/MessageRepo.java
+++ src/main/java/com/example/sweater/repository/MessageRepo.java
@@ -3,8 +3,13 @@ package com.example.sweater.repository;
 import com.example.sweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface MessageRepo extends CrudRepository<Message, Integer> {
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+//    ключевое слово find
+    List<Message> findByTag(String tag);
 }
