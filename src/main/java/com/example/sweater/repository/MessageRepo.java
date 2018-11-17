fcc7fbbf9b5cb7601258c4f698160f8e2d712c4c
alex <akuzin20@gmail.com>
2018-11-17 13:11:32 +0300

    init v5 (+привязка сообщений к их автору(юзеру) использовал OneToMany + UserService

--- src/main/java/com/example/sweater/repository/MessageRepo.java
+++ src/main/java/com/example/sweater/repository/MessageRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called MessageRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
